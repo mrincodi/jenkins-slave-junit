c1fa983313d81939bfd5d09917c2d0a6373b2eec
Ubuntu <ubuntu@ip-172-31-15-19.us-west-1.compute.internal>
2016-09-03 20:49:02 +0000

    Creating user and home dir. so far

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name 'jenkins_slave'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures jenkins_slave'
+long_description 'Installs/Configures jenkins_slave'
+version '0.1.0'
+
+# depends 'apt', '~> 2.9.2'
+depends 'git', '~> 5.0.0'
---
+++ recipes/default.rb
@@ -0,0 +1,2 @@
+include_recipe 'apt::default'
+include_recipe 'jenkins_slave::create_user'
